477360b25ebc3363f1bbd85c1dab5849c3f6e3a3
Amra Mujcinovic <amramujcinovic9@gmail.com>
2019-03-16 23:57:37 +0100

    name and date filter

--- Frontend/src/app/services/event/Event.ts
+++ Frontend/src/app/services/event/Event.ts
@@ -9,6 +9,7 @@ export class Event {
   category: Category;
   place: Place;
   price: number;
+  date_time: Date;
 
   constructor() {
     this.id = null;
@@ -18,5 +19,6 @@ export class Event {
     this.category = new Category;
     this.place = new Place;
     this.price = 0;
+    this.date_time = new Date();
   }
 }
