e8dcdb19d381f763fa7c178fc029897c1cd5280f
Amra Mujcinovic <amramujcinovic9@gmail.com>
2019-04-07 00:24:52 +0200

    pretraga korisnici

--- Frontend/src/app/admin-useri/admin-useri.component.ts
+++ Frontend/src/app/admin-useri/admin-useri.component.ts
@@ -14,9 +14,11 @@ export class AdminUseriComponent implements OnInit {
   users: Array<any>;
   user: User = new User();
   korisnik: User = new User();
-  username_pretraga: any;
+  username_pretraga: string;
   isAdmin = true;
   isLoggedIn = true;
+  searchedUsers: Set<User> = null;
+  selectedDropdow: string;
 
   constructor(
     private router: Router,
@@ -53,5 +55,30 @@ export class AdminUseriComponent implements OnInit {
     this.korisnik.user_role.id = 2;
     this.userService.createUser(this.korisnik).subscribe(data => { });
     window.location.reload();
+  }
+
+  keyUpFunction(event): void {
+
+    if (this.username_pretraga) {
+      this.searchedUsers = new Set<User>();
+      this.users.forEach(element => { this.searchedUsers.add(element); });
+
+      const words = this.username_pretraga.split(' ');
+
+      this.searchedUsers.forEach(element => {
+        let existName = false;
+        words.forEach(word => {
+          if (element.ime.toLowerCase().includes(word.toLowerCase()) || element.prezime.toLowerCase().includes(word.toLowerCase()) || element.username.toLowerCase().includes(word.toLowerCase()) || element.email.toLowerCase().includes(word.toLowerCase())) {
+            existName = true;
+          }
+        });
+        if (existName === false) {
+          this.searchedUsers.delete(element);
+        }
+      });
+    }
+    else {
+      this.searchedUsers = null;
+    }
   }
 }
